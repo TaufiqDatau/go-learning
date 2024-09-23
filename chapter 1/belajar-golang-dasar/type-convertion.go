00fffe8055d212367692fb3bf51014b9b321468d
TaufiqDatau <taufiqhdatau@gmail.com>
2024-09-23 23:33:38 +0700

    string convertion

--- chapter 1/belajar-golang-dasar/type-convertion.go
+++ chapter 1/belajar-golang-dasar/type-convertion.go
@@ -11,4 +11,12 @@ func main(){
   fmt.Println("this is the value of y :",y)
   fmt.Println("this is the value of z :",z)
 
+  var name = "Taufiqurrahman"
+  var e = name[0]
+  var eChar = string(e)
+
+  fmt.Println(name)
+  fmt.Println(e)
+  fmt.Println(eChar)
+
 }
