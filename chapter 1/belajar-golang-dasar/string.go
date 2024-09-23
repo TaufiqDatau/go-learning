e9b1ffcfdbfc58d8ee99c0a501fc009772ff4dd2
TaufiqDatau <taufiqhdatau@gmail.com>
2024-09-23 19:39:54 +0700

    exploring string type

---
+++ chapter 1/belajar-golang-dasar/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+  fmt.Println("Taufiqurrahman")
+  fmt.Println("Taufiqurrahman Hafiidh")
+  fmt.Println("Taufiqurrahman Hafiidh Datau")
+  fmt.Println("Taufiq"[0])
+  fmt.Println(len("Taufiqurrahman"))
+}
