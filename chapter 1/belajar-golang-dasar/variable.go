12a0a9287cbfbf11c4cd50bc629e863af1006ed2
TaufiqDatau <taufiqhdatau@gmail.com>
2024-09-23 19:48:38 +0700

    variable in golang

---
+++ chapter 1/belajar-golang-dasar/variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+  var name string
+
+  name = "Taufiqurrahman"
+  fmt.Println(name)
+
+  name = "Taufiqurrahman Hafiidh Datau"
+  fmt.Println(name)
+
+  var newName = "Taufiq"
+  fmt.Println(newName)
+
+  carType := "Maclaren"
+  fmt.Println(carType)
+}
