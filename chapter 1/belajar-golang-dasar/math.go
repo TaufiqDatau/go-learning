799747f483d84339bd6911a737cea7261c5da34a
TaufiqDatau <taufiqhdatau@gmail.com>
2024-09-24 21:16:48 +0700

    exploring math in golang

---
+++ chapter 1/belajar-golang-dasar/math.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+  var a = 10
+  var b = 10
+  var d = 5
+  var e =2
+  var c = a + b - d*e
+
+  fmt.Println(c)
+
+  a +=10
+  fmt.Println(a)
+  a-=5
+  fmt.Println(a)
+  a*=5
+  fmt.Println(a)
+  
+  a/=5
+  fmt.Println(a)
+
+  a%=4
+  fmt.Println(a)
+}
