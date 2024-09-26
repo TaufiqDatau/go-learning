84d4e8b23a728b1dd8d800bcf7d92125c46aea52
TaufiqDatau <taufiqhdatau@gmail.com>
2024-09-26 23:32:46 +0700

    add for loop material

--- chapter 1/belajar-golang-dasar/for-loop.go
+++ chapter 1/belajar-golang-dasar/for-loop.go
@@ -1,5 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){}
+func main(){
+
+  counter :=1
+  for counter<=10{
+    fmt.Println("Ini iterasi ke ", counter)
+    counter++
+  }
+
+  lsStudent := []string{"Marc","Cathy","Ridel","Nicole","Raya"}
+
+  for _,name := range lsStudent {
+    fmt.Println(name)
+  }
+
+  gameByGenre := map[string][]string{
+    "slice of life":[]string{"Until Then", "A Space For The Unbound"},
+    "fps":[]string{"Warhammer","Counter Strike", "Valorant", "Overwatch"},
+    "Open World":[]string{"Pal World", "Grand Theft Auto"},
+  }
+
+  for key,value := range gameByGenre{
+    fmt.Print(key, " :")
+    for _,gameName := range value{
+      fmt.Println(" ",gameName)
+    }
+  }
+}
