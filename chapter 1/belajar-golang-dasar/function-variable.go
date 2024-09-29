f822fd9e1abda540dc6b66f90dacaae9c311a560
TaufiqDatau <taufiqhdatau@gmail.com>
2024-09-29 18:01:31 +0700

    syntaxtix sugar in golang

--- chapter 1/belajar-golang-dasar/function-variable.go
+++ chapter 1/belajar-golang-dasar/function-variable.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 type stringParam func(string)
+type filterFunction func(val int32, condition int8)bool
 
 func sayHello(name string){
   fmt.Println("Hello", name)
 }
 
-func filterSlice(numberArr []int32, nilaiMinimum int8, filterCondition func(int32, int8)bool) []int32{
+func filterSlice(numberArr []int32, nilaiMinimum int8, filterCondition filterFunction) []int32{
   result := make([]int32, 0, 10)
   for _,value := range numberArr {
     if filterCondition(value, nilaiMinimum){
@@ -23,13 +24,11 @@ func main(){
   containFunction("Jiya")
   nilaiSiswa := []int32{85,43,78,41,80,42,65}
 
-  filter := func(val int32, condition int8)bool{
-    if val >= int32(condition){
-      return true
-    }
-    return false
+  var filter filterFunction = func(val int32, condition int8) bool {
+    return val >= int32(condition)
   }
 
+  
   nilaiSiswa = filterSlice(nilaiSiswa, 80, filter)
 
   fmt.Println(nilaiSiswa, "Menggunakan anonymous function dengan filter ")
