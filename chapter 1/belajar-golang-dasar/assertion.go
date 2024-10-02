8bf1b4835445beb6db56fd3392a78090deaca2d9
TaufiqDatau <taufiqhdatau@gmail.com>
2024-10-02 19:49:03 +0700

    using switch statement for type assertion in golang

---
+++ chapter 1/belajar-golang-dasar/assertion.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func random() any{
+  return "OK"
+}
+
+func main(){
+  result := random()
+  var resultString string = result.(string) // Type Assertion
+  fmt.Println(resultString)
+
+
+  //better way to do type assertion 
+  switch value := result.(type){
+  case string :
+    fmt.Println("This is a string", value)
+  case int :
+    fmt.Println("This is an int", value)
+  default:
+    fmt.Println("unknown")
+  }
+
+}
